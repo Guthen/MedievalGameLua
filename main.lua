d5bfaea8ed61c9756051e2f82ca1d0e7a987cbdc
Guthen <33220603+Guthen@users.noreply.github.com>
2019-01-11 12:41:10 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,9 +1,21 @@
+function RequireFolder(folder)
+    if love.filesystem.getInfo(folder) then
+        for _, v in pairs(love.filesystem.getDirectoryItems(folder)) do
+            local n = string.gsub(v, ".lua", "")
+            require(n)
+        end
+    end
+end
+
+—————————————————————
+
 function love.load()
     w, h = 1280, 720
     title = "MedievalGame"
 
     love.window.setMode(w, h)
     love.window.setTitle(title)
+    RequireFolder("lua")
 end
 
 function love.update(dt)
@@ -16,11 +28,3 @@ end
 
 ————————————————————
 
-function RequireFolder(folder)
-    if love.filesystem.getInfo(folder) then
-        for _, v in pairs(love.filesystem.getDirectoryItems(folder)) do
-            local n = string.gsub(v, ".lua", "")
-            require(n)
-        end
-    end
-end
