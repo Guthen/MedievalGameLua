f5d0d15b0d58bf3c5476fae2a76d29353ce20c3b
Guthen <33220603+Guthen@users.noreply.github.com>
2019-01-11 18:47:29 +0100

    Update image.lua

--- lua/image.lua
+++ lua/image.lua
@@ -4,7 +4,7 @@ function Image:Load()
     if love.filesystem.getInfo("images") then
         for _,v in pairs(love.filesystem.getDirectoryItems("images")) do
             if not string.find(v, ".png") then return end 
-self[string.gsub(v, ".png", "")] = love.graphics.newImage("images/"..v)
+            self[string.gsub(v, ".png", "")] = love.graphics.newImage("images/"..v)
         end
     end
 end
