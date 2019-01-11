33f82bc7bf3c58caa26a8755d6e72b10b35f7c2d
Guthen <33220603+Guthen@users.noreply.github.com>
2019-01-11 12:51:59 +0100

    Update player.lua

--- lua/player.lua
+++ lua/player.lua
@@ -1,1 +1,15 @@
 Player = {}
+
+function Player:Load()
+    self.x = 0
+    self.y = 0
+    self.img = love.graphics.newImage("")
+end
+
+function Player:Update(dt)
+
+end
+
+function Player:Draw()
+    love.graphics.draw(self.img, self.x, self.y)
+end
