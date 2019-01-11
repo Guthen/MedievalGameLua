cd0cc79bddf947b01245d853fe6d928397935dd3
Guthen <33220603+Guthen@users.noreply.github.com>
2019-01-11 12:22:50 +0100

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,13 @@
+function love.load()
+    w, h = 1280, 720
+    love.window.setMode(w, h)
+    love.window.setTitle("Medieval Game")
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+
+end
