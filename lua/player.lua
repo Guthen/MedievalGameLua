eca52225f0c91607874db379cda49234d6a02684
Guthen <33220603+Guthen@users.noreply.github.com>
2019-01-15 18:19:53 +0100

    Added : New Images & Auto-Map-Smooth and Better Collisions

--- lua/player.lua
+++ lua/player.lua
@@ -4,16 +4,27 @@ function Player:Load()
     self.x = 0
     self.y = 0
     self.img = Image["player"]
+
+    self.BlockMapID = 
+    {
+        [0] = false,
+        [1] = true,
+        [2] = true,
+        [3] = true,
+        [4] = false,
+        [5] = false,
+        [6] = true,
+    }
 end
 
 function Player:Key(k)
-    if k == "z" and Map[self.y+1-1] and Map[self.y+1-1][self.x+1] == 0 then
+    if k == "z" and Map[self.y+1-1] and not self.BlockMapID[Map[self.y+1-1][self.x+1]] then
         self.y = self.y - 1
-    elseif k == "s" and Map[self.y+1+1] and Map[self.y+1+1][self.x+1] == 0 then
+    elseif k == "s" and Map[self.y+1+1] and not self.BlockMapID[Map[self.y+1+1][self.x+1]] then
         self.y = self.y + 1
-    elseif k == "q" and Map[self.y+1] and Map[self.y+1][self.x+1-1] == 0 then
+    elseif k == "q" and Map[self.y+1] and not self.BlockMapID[Map[self.y+1][self.x+1-1]] then
         self.x = self.x - 1
-    elseif k == "d" and Map[self.y+1] and Map[self.y+1][self.x+1+1] == 0 then
+    elseif k == "d" and Map[self.y+1] and not self.BlockMapID[Map[self.y+1][self.x+1+1]] then
         self.x = self.x + 1
     end
 end
