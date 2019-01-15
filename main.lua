b752ae8ec87beb0867ac2aee2627a63a01ec056c
Guthen <33220603+Guthen@users.noreply.github.com>
2019-01-15 17:27:10 +0100

    Bug fix

--- main.lua
+++ main.lua
@@ -1,13 +1,17 @@
+io.stdout:setvbuf("no")
+
 -- FONCTION POUR IMPORTER LES FICHIERS ".lua" D’UN DOSSIER
 function RequireFolder(folder)
     if love.filesystem.getInfo(folder) then
         for _, v in pairs(love.filesystem.getDirectoryItems(folder)) do
             if not string.find(v, ".lua") then return end
             local n = string.gsub(v, ".lua", "")
-            require(n)
+            require(folder.."/"..n)
         end
     end
 end
+
+-- FONCTIONS PRINCIPALES LOVE
 
 function love.load()
     w, h = 1280, 720
@@ -15,6 +19,8 @@ function love.load()
 
     love.window.setMode(w, h)
     love.window.setTitle(title)
+    love.graphics.setDefaultFilter("nearest")
+    
     RequireFolder("lua")
     
     Image:Load()
@@ -32,6 +38,8 @@ function love.keypressed(k)
 end
 
 function love.draw()
+    love.graphics.scale(2, 2)
+
     Map:Draw()
     Player:Draw()
 end
