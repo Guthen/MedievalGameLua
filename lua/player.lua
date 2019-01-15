ee19730eecc0b13467aa8db4b437b91be44d71f1
Guthen <33220603+Guthen@users.noreply.github.com>
2019-01-15 12:03:19 +0100

    Update player.lua

--- lua/player.lua
+++ lua/player.lua
@@ -6,6 +6,18 @@ function Player:Load()
     self.img = love.graphics.newImage("")
 end
 
+function Player:Key(k)
+    if k == "z" then
+        self.y = self.y - 1
+    elseif k == "s" then
+        self.y = self.y + 1
+    elseif k == "q" then
+        self.x = self.x - 1
+    elseif k == "d" then
+        self.x = self.x + 1
+    end
+end
+
 function Player:Update(dt)
 
 end
