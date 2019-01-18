439a665cfb046f1dc58659d5b1ddd780236b3545
Guthen <33220603+Guthen@users.noreply.github.com>
2019-01-18 12:58:48 +0100

    Update player.lua : Map system

--- lua/player.lua
+++ lua/player.lua
@@ -11,22 +11,22 @@ end
 
 function Player:Key(k)
     if not self.canMove then return end
-    if k == "z" and Map[self.y+1-1] and not BlockMapID[Map[self.y+1-1][self.x+1]] then
+    if k == "z" and self.levels[self.curMap][self.y+1-1] and not BlockMapID[self.levels[self.curMap][self.y+1-1][self.x+1]] then
         self.y = self.y - 1
         self.canMove = false
         Timer:Add(self.delayMove, function() self.canMove = true end)
         Bot:UpdateMove()
-    elseif k == "s" and Map[self.y+1+1] and not BlockMapID[Map[self.y+1+1][self.x+1]] then
+    elseif k == "s" and self.levels[self.curMap][self.y+1+1] and not BlockMapID[self.levels[self.curMap][self.y+1+1][self.x+1]] then
         self.y = self.y + 1
         self.canMove = false
         Timer:Add(self.delayMove, function() self.canMove = true end)
         Bot:UpdateMove()
-    elseif k == "q" and Map[self.y+1] and not BlockMapID[Map[self.y+1][self.x+1-1]] then
+    elseif k == "q" and self.levels[self.curMap][self.y+1] and not BlockMapID[self.levels[self.curMap][self.y+1][self.x+1-1]] then
         self.x = self.x - 1
         self.canMove = false
         Timer:Add(self.delayMove, function() self.canMove = true end)
         Bot:UpdateMove()
-    elseif k == "d" and Map[self.y+1] and not BlockMapID[Map[self.y+1][self.x+1+1]] then
+    elseif k == "d" and self.levels[self.curMap][self.y+1] and not BlockMapID[self.levels[self.curMap][self.y+1][self.x+1+1]] then
         self.x = self.x + 1
         self.canMove = false
         Timer:Add(self.delayMove, function() self.canMove = true end)
